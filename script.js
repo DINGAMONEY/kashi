0eecf482e227efd56c46463f207f53672abc27b1
DINGAMONEY <qlatif362@gmail.com>
2018-04-23 20:13:32 +0500

    Created

---
+++ script.js
@@ -0,0 +1,10 @@
+function rdbg(){
+	document.getElementById('main').style.background = "red";
+}
+function grbg(){
+	document.getElementById('main').style.background = "green";
+}
+function darkmode(){
+	document.body.style.background = "#ccc";
+	document.getElementById('main').style.background = "#000";
+}
